9c77beb0df5d0e611a8b8b352b0e760fda77becf
baruna <baruna@NEBULA>
2022-03-29 10:03:57 +0700

    act 5(2)

---
+++ app/src/main/java/com/example/sampleconstrainlayout/ListViewAdapter.java
@@ -0,0 +1,36 @@
+package com.example.sampleconstrainlayout;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ListViewAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+
+    }
+}
+//Deklarasi variabel dengan jenis data context
+Context mContext;
+
+//Deklarasi variabel dengan jenis data layout inflater
+LayoutInflater inflater;
+
+//Deklarasi variabel dengan jenis data arrayList
+private ArrayList<classNama> arraylist;
---
+++ app/src/main/java/com/example/sampleconstrainlayout/home_activity.java
@@ -0,0 +1,6 @@
+package com.example.sampleconstrainlayout;
+
+import android.app.Activity;
+
+public class home_activity extends Activity {
+}
