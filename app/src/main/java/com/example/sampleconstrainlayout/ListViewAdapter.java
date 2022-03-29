1d71dc16204fd12bc90a239bb11a4d699e189551
baruna <baruna@NEBULA>
2022-03-29 11:01:52 +0700

    act 5(3)

--- app/src/main/java/com/example/sampleconstrainlayout/ListViewAdapter.java
+++ app/src/main/java/com/example/sampleconstrainlayout/ListViewAdapter.java
@@ -1,36 +1,94 @@
 package com.example.sampleconstrainlayout;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ListViewAdapter extends BaseAdapter {
-    @Override
-    public int getCount() {
-        return 0;
+    //Deklarasi variabel dengan jenis data context
+    Context mContext;
+
+    //Deklarasi variabel dengan jenis data layout inflater
+    LayoutInflater inflater;
+
+    //Deklarasi variabel dengan jenis data arrayList
+    private ArrayList<ClassNama> arraylist;
+
+    //Membuat konstruktor Listviewadapter
+    public ListViewAdapter(Context context) {
+        //Memberi nilai mContext dengan context
+        mContext = context;
+
+        //mengatur Layoutinflater dari context yang diberikan
+        inflater = LayoutInflater.from(mContext);
+
+        //Memberikan nilai arrayList dari class ClassNama
+        this.arraylist = new ArrayList<ClassNama>();
+
+        //Menambahkan semua elemen ke arraylist
+        this.arraylist.addAll(home_activity.classNamaArrayList);
+    }
+
+    //Membuat class untuk mendeklarasikan tempat untuk meletakkan isi kedalam Listview
+    public class ViewHolder {
+        //mendeklarasikan variabel dengan jenis TextView
+        TextView name;
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
+    public int getCount() {
+        //mengembalikan nilai berupa jumlah data dari array data
+        return home_activity.classNamaArrayList.size();
     }
+
+    //Fungsi ini digunakan untuk mendapatkan data item yang terkait dengan
+    // tertentu dalam kumpulan data untuk mendapatkan data yang sesuai dari
+    // tertentu dalam pengumpulan data item.
+    @Override
+    public Object getItem(int i) {
+        //mengembalikan nilai berupa objek dari array data
+        return home_activity.classNamaArrayList.get(i);
+    }
+    //Fungsi mengembalikan "nilai" dari posisi item ke adaptor
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
+    //Fungsi ini secara otomatis dipanggil ketika tampilan item list siap untuk
+    // ditampilkan atau akan ditampilkan
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        //deklarasi variabel dengan jenis data ViewHolder
+        final ViewHolder holder;
+        //Membuat kondisi apakah view null atau tidak
+        if (view == null) {
+            //membuat objek view holder
+            holder = new ViewHolder();
 
+            //Mengatur Layout untuk menampilkan item
+            view = inflater.inflate(R.layout.item_listview, null);
+
+            // set id untuk textview
+            holder.name = (TextView) view.findViewById(R.id.tvnama_item);
+
+            //menyimpan data dalam tampilan tanpa menggunakan struktur data lain.
+            view.setTag(holder);
+        } else {
+            //mengatur holder untuk mengembalikan nilai dari view tag.
+            holder = (ViewHolder) view.getTag();
+        }
+        // Set item ke TextViews
+        holder.name.setText(home_activity.classNamaArrayList.get(i).getName());
+        //mengembalikan nilai ke variabel view
+        return view;
     }
 }
-//Deklarasi variabel dengan jenis data context
-Context mContext;
 
-//Deklarasi variabel dengan jenis data layout inflater
-LayoutInflater inflater;
 
-//Deklarasi variabel dengan jenis data arrayList
-private ArrayList<classNama> arraylist;
--- app/src/main/java/com/example/sampleconstrainlayout/home_activity.java
+++ app/src/main/java/com/example/sampleconstrainlayout/home_activity.java
@@ -1,6 +1,111 @@
 package com.example.sampleconstrainlayout;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.icu.text.Transliterator;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
 import android.app.Activity;
 
-public class home_activity extends Activity {
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class home_activity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+    //Deklarasi variabel dengan jenis data Listview
+    private ListView list;
+
+    //Memanggil class ListviewAdapter dan diinisiasi menjadi variabel adapter
+    private ListViewAdapter adapter;
+
+    //Deklarasi array untuk menyimpan nama
+    String[] listNama;
+
+    //Memanggil class CmassNama
+    public static ArrayList<ClassNama> classNamaArrayList = new ArrayList<ClassNama>();
+
+    //Membuat objek Bundle
+    Bundle bundle = new Bundle();
+
+    //Membuat objek intent
+    Intent intent;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //Menyimpan nama didalam array ListNama
+        listNama = new String[]{"Inayah", "Ilham", "Eris ",
+                "Fikri", "Maul", "Intan", "Vina", "Gita",
+                "Vian", "Lutfi"};
+
+        list = findViewById(R.id.listKontak);
+
+        //membuat objek dari class ClassNama menjadi arraylist
+        classNamaArrayList = new ArrayList<>();
+
+        //Membaca seluruh data pada array ListNama
+        for (int i = 0; i < listNama. length; i++)
+        {
+            //Membuat objek class nama da
+            ClassNama classNama = new ClassNama(listNama[i]);
+            // Binds strings ke array
+            classNamaArrayList.add(classNama);
+        }
+
+        // Membuat objek dari ListviewAdapter
+        adapter = new ListViewAdapter(this);
+
+        // Binds Adapter ke ListView
+        list.setAdapter(adapter);
+
+        //Membuat event dari List onclick
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+
+            //Deklarasi variabel nama yang berisi item yang diklik
+            String nama = classNamaArrayList.get(position).getName();
+
+            //memsukkan volue dori variabel nama dengan kunci "a"
+            // dan dimasukkan kedalam bundle
+            bundle.putString("a", nama.trim());
+
+            //Membuat objek popup menu
+            PopupMenu pm = new PopupMenu(getApplicationContext(),View);
+
+            // Membuat event untuk popup menu ketika dipilih
+            pm.setOnMenuItemClickListener(home_activity.this);
+
+            //Menampilkan popup menu dari Layout menu
+            pm.inflate(R.menu.popup_menu);
+
+            //Menampilkan popup menu
+            pm.show();
+        }
+    });
+}
+//event yang terjadi ketika menu dipilih
+@Override
+public boolean onMenuItemClick(MenuItem menuItem) {
+    switch (menuItem.getItemId())
+    {
+
+        case R.id.mnview:
+            intent = new Intent(getApplicationContext(), ActivityLihatData.class);
+            intent.putExtras (bundle);
+            startActivity(intent);
+            break;
+            case R.id.mnedit:
+        Toast.makeText(getApplicationContext(), "Ini untuk edit kontak",
+                Toast.LENGTH_LONG).show();
+        break;
+    }
+    return false;
+}
 }
